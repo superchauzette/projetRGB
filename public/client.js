4005821db52ab4097e6afd6e06fdb2b01e021c1c
unknown <u134898@PC-G1022881.encara.local.ads>
2015-02-26 10:25:16 +0100

    changement de fonction hsl a hsv

--- public/client.js
+++ public/client.js
@@ -1,29 +1,29 @@
 $(function() {
 
 	var widthTotal = $('body')[0].clientWidth;
-	var x = -5;
-	xsl2rgb(x,100,50);	
+	var x = 0;
+	xsv2rgb(x,100,50);	
 
 	$( "#draggable" ).draggable({
 		scroll: false, 
 		//axis: "x" ,
 		stop: function(d) {
-			x =  Math.round( (d.clientX/widthTotal)*100 - 5 );
-			xsl2rgb(x,100,50);	
+			x =  Math.round( (d.clientX/widthTotal)*100 );
+			xsv2rgb(x,100,50);	
 		}	 
 	});
 
 	$('#lumisosite').on("change", function(d){			
-		xsl2rgb( x, 100, $(this)[0].value );
+		xsv2rgb( x, 100, $(this)[0].value );
 	});
 	$("#btnoff").on("click", function(){
 		$('#lumisosite')[0].value = 0;
-		xsl2rgb(0,0,0);
+		xsv2rgb(0,0,0);
 	})
 
 	$("#btnon").on("click", function(){
 		$('#lumisosite')[0].value = 100;
-		xsl2rgb(0,0,100);
+		xsv2rgb(0,0,100);
 	})
 
 });
@@ -35,69 +35,42 @@ function pwm(red, green, blue){
     });
 }
 
-function xsl2rgb(x,s,l){
-	var hsl = [ x*360/100 , s, l ];
-	var rgb = hsl2rgb(hsl);
+function xsv2rgb(x,s,v){
+	var rgb = hsv2rgb( [x*360/100 , s, v] );
+	var r = Math.round(rgb[0]);
+	var g = Math.round(rgb[1]);
+	var b = Math.round(rgb[2]);
 
-	$('#info')
-		.html( x+5 +"% </br> lumisosite : "+l + "%")
-		.css("background", "rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")");
+	$('#info').html( x +"% </br> lumisosite : "+ v + "%")
+			  .css("background-color", "rgb("+ r +","+ g +","+ b +")" );
 
-	pwm(rgb[0], rgb[1], rgb[2]);
+	//pwm(rgb[0], rgb[1], rgb[2]);
 }
 
-function hsl2rgb(hsl) {
-  var h = hsl[0] ,
-      s = hsl[1] ,
-      l = hsl[2];
+function hsv2rgb(hsv) {
+  var h = hsv[0] / 60,
+      s = hsv[1] / 100,
+      v = hsv[2] / 100,
+      hi = Math.floor(h) % 6;
 
-  var r, g, b, m, c, x
+  var f = h - Math.floor(h),
+      p = 255 * v * (1 - s),
+      q = 255 * v * (1 - (s * f)),
+      t = 255 * v * (1 - (s * (1 - f))),
+      v = 255 * v;
 
-  if (!isFinite(h)) h = 0
-  if (!isFinite(s)) s = 0
-  if (!isFinite(l)) l = 0
-
-  h /= 60
-  if (h < 0) h = 6 - (-h % 6)
-  h %= 6
-
-  s = Math.max(0, Math.min(1, s / 100))
-  l = Math.max(0, Math.min(1, l / 100))
-
-  c = (1 - Math.abs((2 * l) - 1)) * s
-  x = c * (1 - Math.abs((h % 2) - 1))
-
-  if (h < 1) {
-    r = c
-    g = x
-    b = 0
-  } else if (h < 2) {
-    r = x
-    g = c
-    b = 0
-  } else if (h < 3) {
-    r = 0
-    g = c
-    b = x
-  } else if (h < 4) {
-    r = 0
-    g = x
-    b = c
-  } else if (h < 5) {
-    r = x
-    g = 0
-    b = c
-  } else {
-    r = c
-    g = 0
-    b = x
+  switch(hi) {
+    case 0:
+      return [v, t, p];
+    case 1:
+      return [q, v, p];
+    case 2:
+      return [p, v, t];
+    case 3:
+      return [p, q, v];
+    case 4:
+      return [t, p, v];
+    case 5:
+      return [v, p, q];
   }
-
-  m = l - c / 2
-  r = Math.round((r + m) * 255)
-  g = Math.round((g + m) * 255)
-  b = Math.round((b + m) * 255)
-
-  return [ r,g,b ]
 }
-
